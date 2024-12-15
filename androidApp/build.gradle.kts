20acd7fda2bc2c82851fdc59f03e952b1be50e35
Shadat Rahman <43848931+mdshadatrahman@users.noreply.github.com>
2024-12-16 00:51:31 +0600

    android app done.

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "com.example.demo.android"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.demo.android"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.5"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(projects.shared)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    implementation(libs.androidx.activity.compose)
+    debugImplementation(libs.compose.ui.tooling)
+}
---
+++ shared/src/commonMain/kotlin/com/example/demo/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.demo
+
+object Constants {
+    const val BASE_URL = "https://fakestoreapi.com/"
+}
