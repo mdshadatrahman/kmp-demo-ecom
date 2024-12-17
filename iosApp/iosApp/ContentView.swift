3c76a596e9085fd46649f18af5d94782035327c6
Shadat Rahman <43848931+mdshadatrahman@users.noreply.github.com>
2024-12-17 23:28:52 +0600

    ios app done

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,10 +2,36 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-
+    @StateObject
+    var viewModel = HomeViewModel()
+    
 	var body: some View {
-		Text(greet)
+        VStack {
+            if viewModel.response?.isSuccess() == true {
+                List(viewModel.response?.getProducts().items ?? [], id: \.id) {element in
+                    ProductView(product: element)
+                }
+            } else if viewModel.response?.isError() == true {
+                VStack {
+                    Spacer()
+                    Text(viewModel.response?.getErrorMessage() ?? "Something went wrong")
+                        .font(.title3.bold())
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else if viewModel.response?.isLoading() == true {
+                VStack {
+                    Spacer()
+                    ProgressView("Loading")
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .padding()
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }.task {
+            await viewModel.fetchData()
+        }
 	}
 }
 
@@ -13,4 +39,17 @@ struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
 	}
+}
+
+
+class HomeViewModel: ObservableObject {
+    @Published
+    private(set) var response: RequestState? = nil
+    
+    @MainActor
+    func fetchData() async {
+        for await requestState in ProductsApi().fetchProducts(limit: 10){
+            response = requestState
+        }
+    }
 }
